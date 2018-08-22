066b93461df3c8bb4305b3828670471e8473e3bb
Anton Panteleev <anton@panteleev.net>
2018-08-22 14:25:07 +0300

    Missed close div tag

--- src/details.js
+++ src/details.js
@@ -25,7 +25,7 @@ export function getDetailsContentLayout(ymaps) {
                 Идет загрузка данных...
             </div>
         {% endif %}
-    `,
+    </div>`,
     {
       build: () => {
         BalloonContentLayout.superclass.build.call(this);
